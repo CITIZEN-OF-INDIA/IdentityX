b7505560911c4076b5bc9c49ed796dd603fa7be0
CITIZEN-OF-INDIA <jyoti9313321050@gmail.com>
2026-06-07 14:56:22 +0530

    NEW CHANGES

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://citizen-of-india-my-projects.hf.space/docs",
+  baseURL: "https://citizen-of-india-my-projects.hf.space/recognise",
 });
 
 export default API;
