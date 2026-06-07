f17feda39bca6c3ff6915fed92941475267f1836
CITIZEN-OF-INDIA <jyoti9313321050@gmail.com>
2026-06-07 14:53:32 +0530

    NEW FRONTEND CHANGES WITH HUGGING FACES

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://citizen-of-india-my-projects.hf.space/docs",
 });
 
 export default API;
